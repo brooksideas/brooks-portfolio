7e4e480d9141915752c7bf9545672f421a0258dd
Brook Tarekegn <brooksideas@gmail.com>
2022-03-10 20:51:08 +0300

    structure: Setup the main TS and Eslint tools for Vue3

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      node: true,
+      browser: true,
+      es6: true,
+      jest: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    globals: {},
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+    plugins: ["vue", "import"],
+    ignorePatterns: ["node_modules/"],
+    rules: {},
+    settings: {
+      VTTCue: {
+        version: "latest", // "detect" automatically picks the version you have installed.
+      },
+    },
+  };
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    hello, Brook
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+});
+</script>
+
+<style>
+
+</style>
